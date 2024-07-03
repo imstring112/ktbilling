ffd9033d9f45ba7fa2101a52031188c867b68d78
imstring112 <marcelooprogramador@gmail.com>
2024-07-03 19:30:07 -0300

    feat(wip): configured pest to test

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\BillingController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('API/V1')->post('billing', [BillingController::class, 'proccess']);
+
+
+
+
